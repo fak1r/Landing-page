9904d6db1932425d53d45dbfcc93f37c04d87487
fak1r <fak1r@yandex.ru>
2023-07-07 17:16:29 +0300

    Добавлен Js для Burger menu и faq accordion

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,61 @@
-console.log('test');
+const header =  document.querySelector('.section-header');
+const mainNavigation = document.getElementById('main-navigation');
+
+document.querySelector('.faq-accordion')
+    .addEventListener('click', (event) => {
+    if (event.target.closest('.faq-accordion__item')) {
+        event.target.closest('.faq-accordion__item')
+            .classList.toggle('faq-accordion__item--active')
+    }
+});
+
+document.querySelector('.btn-burger').addEventListener('click', () => {
+
+    header.classList.toggle('section-header--active-nav');
+
+    if (header.classList.contains('section-header--active-nav')){
+        hideScroll();
+    } else {
+        showScroll();
+    }
+});
+
+const hideScroll = () => {
+    const scrollWidth = `${getScrollbarWidth()}px`;
+
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
+    mainNavigation.style.paddingRight = scrollWidth;
+
+};
+
+const showScroll = () => {
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
+
+    mainNavigation.style.paddingRight = '';
+};
+
+const resetNav = () => {
+    header.classList.remove('section-header--active-nav');
+    showScroll();
+}
+
+window.addEventListener('resize', resetNav)
+
+const getScrollbarWidth = () => {
+    const outer = document.createElement('div');
+
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
+
+    document.body.appendChild(outer);
+    const scrollBarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
+
+    return scrollBarWidth;
+}
